667e19087459623c285365ed15db384e51eaf9f6
Luca Visentin <visentin.hl@gmail.com>
2022-09-22 16:26:13 +0200

    Initial commit

---
+++ src/db_rebuilder/daedalus/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+OUT_ANCHOR: Path = Path("/app/out")
+
+__all__ = ["OUTANCHOR"]
+__version__ = "0.1.0"
---
+++ src/db_rebuilder/daedalus/__main__.py
@@ -0,0 +1,6 @@
+import os
+from daedalus import OUT_ANCHOR
+
+print("Hello, world!")
+
+print(os.listdir(OUT_ANCHOR))
