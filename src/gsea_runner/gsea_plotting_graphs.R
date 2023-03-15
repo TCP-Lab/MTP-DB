65ff931f908a18d47c1bdbb05ca4406f96c65e2d
Luca Visentin <visentin.hl@gmail.com>
2023-03-15 11:50:59 +0100

    Added some plotting graphs

--- src/gsea_runner/gsea_plotting_graphs.R
+++ src/gsea_runner/gsea_plotting_graphs.R
@@ -1,4 +1,3 @@
-
 library(tidyverse)
 requireNamespace("RColorBrewer")
 requireNamespace("igraph")
@@ -60,15 +59,15 @@ name_to_layer <- function(x, layers) {
 result_to_graph <- function(result, base_edges, layers) {
   # This fun gets a results list (w/o plots) and converts it to a dataframe
   # that can be used by ggraph and igraph
-  
+
   # First, we need to see if the base edges are in the results
   req_nodes <- unique(unlist(base_edges))
   id_nodes <- result$pathway[endsWith(result$pathway, "id.txt")]
   available_nodes <- unique(sapply(id_nodes, \(x){str_split_1(x, "~")[1]}))
-  
+
   assert(all(req_nodes %in% available_nodes), "All base nodes are available")
   # Ok, now we can add the edges to the resulting graph.
-  
+
   res <- list()
   # We need all non-id nodes
   for (item in result$pathway[! endsWith(result$pathway, "id.txt")]) {
@@ -76,12 +75,12 @@ result_to_graph <- function(result, base_edges, layers) {
   }
   res <- do.call(rbind.data.frame, res)
   colnames(res) <- c(1, 2)
-  
+
   assert(all(unique(res[,1]) %in% req_nodes), "All nodes are children of base nodes")
-  
+
   # We can now build the frame
   edge_frame <- rbind(setNames(base_edges, names(res)), res)
-  
+
   vres <- list()
   vertices <- unique(unlist(edge_frame))
   for (i in seq_along(vertices)) {
@@ -96,10 +95,10 @@ result_to_graph <- function(result, base_edges, layers) {
   }
   vertice_frame <- do.call(rbind.data.frame, vres)
   colnames(vertice_frame) <- c(1, "NES", "padj")
-  
+
   # We can now add the layers that the vertices will sit in
   vertice_frame$layers <- sapply(vertice_frame[[1]], name_to_layer, layers = layers)
-  
+
   return(igraph::graph_from_data_frame(edge_frame, vertices = vertice_frame))
 }
 
@@ -107,11 +106,11 @@ result_to_graph <- function(result, base_edges, layers) {
 
 make_colours <- function(palette, values) {
   colour_fun <- colorRamp(palette)
-  
+
   values <- (values - min(values)) / (max(values) - min(values))
-  
+
   col_values <- colour_fun(values)
-  
+
   colours <- apply(col_values, 1, function(x) {
     rgb(x[1], x[2], x[3], maxColorValue = 255)
   })
@@ -120,19 +119,39 @@ make_colours <- function(palette, values) {
 parse_name_to_label <- function(x) {
   sapply(x, \(x){
     splits <- str_split_1(x, "~")
-    if (length(splits) == 1) {
-      return(splits[1])
+    value <- if (length(splits) == 1) {
+      splits[1]
     } else {
-      return(paste0(splits[2], ": ", str_remove(splits[3], ".txt")))
+      paste0(splits[2], ": ", str_remove(splits[3], ".txt"))
     }
+
+    if (startsWith(value, "carried_solute")) {
+      value <- str_remove(value, "carried_solute: ")
+    }
+
+    replacements <- c(
+      "outward", "inward", "voltage independent", "voltage gated", "not LG", "ligand gated",
+      "voltage independent", "voltage gated", "not LG", "LG", ""
+    )
+    names(replacements) <- c(
+      "direction: out", "direction: in", "is_voltage_gated: 0", "is_voltage_gated: 1",
+      "is_ligand_gated: 0", "is_ligand_gated: 1", "is_voltage_gated: 0.0", "is_voltage_gated: 1.0",
+      "is_ligand_gated: 0.0", "is_ligand_gated: 1.0", "whole_transportome"
+    )
+
+    if (value %in% names(replacements)) {
+      value <- replacements[value]
+    }
+
+    value
   })
 }
 
 get_point_coords <- function(p) {
   ggp <- ggplot_build(p)
-  
+
   return(ggp$data[[1]][, c("label", "x", "y")])
-  
+
 }
 
 calculate_angle_from_pos <- function(pos_dataframe, specials = NULL) {
@@ -142,71 +161,71 @@ calculate_angle_from_pos <- function(pos_dataframe, specials = NULL) {
 
   # Change the positions to be slightly more outward
   # 1. we calculate the hypothenuse + the dodge value
-  scaling_factor <- 0.015
+  scaling_factor <- 0.01
   new_coords <- apply(pos_dataframe, 1, \(row) {
     name <- row["label"]; y <- as.numeric(row["y"]); angle <- as.numeric(row["angle"])
-    # It's important that we 
-    x <- as.numeric(row["x"]); 
-    hypothenuse <- sqrt(x ** 2 + y ** 2) + (max(str_length(name), 12) * scaling_factor)
+    # It's important that we
+    x <- as.numeric(row["x"]);
+    hypothenuse <- sqrt(x ** 2 + y ** 2) + (min(max(str_length(name), 5), 15) * scaling_factor)
     if (y < 0) {
       new_y <- hypothenuse * abs(sin(angle)) * - 1
     } else {
       new_y <- hypothenuse * abs(sin(angle))
     }
-    
+
     if (x < 0) {
       new_x <- hypothenuse * abs(cos(angle)) * -1
     } else {
       new_x <- hypothenuse * abs(cos(angle))
     }
-    
+
     return(c(new_x, new_y))
   })
-  
+
   pos_dataframe$dodged_x <- new_coords[1,] # it is filled row-wise
   pos_dataframe$dodged_y <- new_coords[2,]
-  
+
   # From radians to degrees
-  pos_dataframe$angle <- pos_dataframe$angle * 180 / pi 
-  
+  pos_dataframe$angle <- pos_dataframe$angle * 180 / pi
+
   # Detect which labels do not lay on the outer circle, so that we can
   # label them differently
   hypothenuses <- sqrt(pos_dataframe$x ** 2 + pos_dataframe$y ** 2)
   pos_dataframe[hypothenuses - max(hypothenuses) > max(hypothenuses) * 0.001,] |> print()
-  
+
   pos_dataframe
 }
 
 plot_result <- function(result, base_edges, layers) {
-  
+
   data_graph <- result_to_graph(result, base_edges, layers)
-  colours <- make_colours(c("blue", "grey", "red"), as.numeric(igraph::vertex_attr(data_graph, "NES")))
-  
+  colours <- make_colours(c("blue", "gray", "red"), as.numeric(igraph::vertex_attr(data_graph, "NES")))
+
   # "Plot" a graph with just the labels
-  p <- ggraph(data_graph, layout='dendrogram', circular = TRUE) +
+  p <- ggraph(data_graph, layout='igraph', algorithm = "tree", circular = TRUE) +
     coord_fixed() +
     geom_node_text(
       aes(
         label = parse_name_to_label(igraph::vertex_attr(data_graph, "name"))
       )
     )
-  
+
   plot_labels <- calculate_angle_from_pos(get_point_coords(p))
-  
-  expand_vec <- c(0.15, 0.15)
-  
+
+  expand_vec <- c(0.05, 0.05)
+
   # Now we have the angles, we can build the real plot
-  pp <- ggraph(data_graph, layout='dendrogram', circular = TRUE) +
+  pp <- ggraph(data_graph, layout='igraph', algorithm = "tree", circular = TRUE) +
     geom_edge_diagonal(aes(alpha = after_stat(index)), show.legend = FALSE) +
     coord_fixed() +
     scale_edge_colour_distiller(palette = "RdPu") +
     geom_node_point(
       aes(
-        size = as.numeric(igraph::vertex_attr(data_graph, "padj")),
-        color = as.numeric(igraph::vertex_attr(data_graph, "NES")),
+        size = -log10(as.numeric(igraph::vertex_attr(data_graph, "padj"))),
+        color = colours,
         alpha = as.numeric((as.numeric(igraph::vertex_attr(data_graph, "padj")) < 0.05) + 1) / 2
       ),
-      show.legend = setNames(c(TRUE, FALSE, FALSE), c("color", "size", "alpha"))
+      show.legend = setNames(c(FALSE, FALSE, FALSE), c("color", "size", "alpha"))
     ) +
     geom_node_text(
       aes(
@@ -216,12 +235,12 @@ plot_result <- function(result, base_edges, layers) {
       ), angle = plot_labels$angle,
       size = 2.5
     ) +
-    scale_color_continuous(guide = guide_legend(title = "NES")) +
+    scale_color_manual(values = colours, limits = colours, guide = guide_legend(title = "NES")) +
     theme(legend.position = "bottom", panel.background = element_blank()) +
     # Give more space to the plot area so the lables are drawn properly
     scale_x_continuous(expand = expand_vec) + scale_y_continuous(expand = expand_vec) +
     ggtitle("A plot of nice data")
-  
+
   return(pp)
 }
 
