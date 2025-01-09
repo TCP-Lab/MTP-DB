bf102f907433e80bc7e7821675e6b4428296c7f5
Luca Visentin <luca.visentin@unito.it>
2025-01-09 16:06:57 +0100

    fix: add post_build hook to delete spurious items in aquaporins and co

---
+++ src/daedalus/post_build_hooks/090_delete_spurius_aquaporins.sql
@@ -0,0 +1,27 @@
+-- This removes some aquaporins that are included in the channels list
+-- probably by the GO
+DELETE FROM channels WHERE ensg IN (
+	"ENSG00000086159",
+	"ENSG00000103375",
+	"ENSG00000103569",
+	"ENSG00000135517",
+	"ENSG00000143595",
+	"ENSG00000161798",
+	"ENSG00000165269",
+	"ENSG00000165272",
+	"ENSG00000167580",
+	"ENSG00000171885",
+	"ENSG00000178301",
+	"ENSG00000240583"
+);
+
+-- This removes some SLCs that the GO introduces in the channels table
+DELETE FROM channels WHERE ensg IN (
+	SELECT ensg FROM solute_carriers
+);
+
+-- Same for pumps and ABCs
+DELETE FROM channels WHERE ensg IN (
+	SELECT ensg FROM pumps
+);
+
