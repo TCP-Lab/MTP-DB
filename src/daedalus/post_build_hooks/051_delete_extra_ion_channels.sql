3840ded7f45d4eb975edff479dd9a9902b8e85a3
Luca Visentin <luca.visentin@unito.it>
2026-01-03 12:20:44 +0100

    fix: Fix #50 by adding duplicated redundant ENSGs

--- src/daedalus/post_build_hooks/051_delete_extra_ion_channels.sql
+++ src/daedalus/post_build_hooks/051_delete_extra_ion_channels.sql
@@ -2,5 +2,7 @@
 -- These are SLC5A8 and SLC9C1 that are wrongly included in the channels list.
 DELETE FROM channels WHERE ensg IN (
     "ENSG00000256870",
+    "ENSG00000262217",
+    "ENSG00000285044",
     "ENSG00000172139"
 );
